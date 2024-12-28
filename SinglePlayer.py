f134866eb6c240676510295e5f74e4b4a75d70e9
stefankablowski <stefan.kablowski@gmail.com>
2024-12-28 01:14:24 +0100

    tile mirror, switch and ux improvements

--- SinglePlayer.py
+++ SinglePlayer.py
@@ -16,14 +16,17 @@ def main(stdscr):
     x = 0
     y = 0
     
+    tile_index = 0
+    
     game = Game()
     game.init_game()
     grid = game.grid
     
-    tile_matrix = [
-            [True, True],
-            [True, False]
-        ]
+    # determine the player of the chosen color
+    # get player of the game players with the same color as the chosen color
+    player = [player for player in game.players if player.color == chosen_color][0]
+    
+    i, tile_matrix = player.hand[tile_index]
 
     # Hide the cursor
     curses.curs_set(0)
@@ -50,10 +53,22 @@ def main(stdscr):
             x = max(0, x - 1)
         elif key == curses.KEY_DOWN:
             x = min(curses.LINES - 1, x + 1)
-        elif key == ord(' '):
+        elif key == ord(' ') or key == ord('r'):
             tile_matrix = Tile.rotate_tile(tile_matrix, 1)
         elif key == curses.KEY_ENTER or key == 10:
-            grid.add_tile(chosen_color, (x, y), 1, tile_matrix)
+            grid.add_tile(chosen_color, (x, y), tile_index, tile_matrix)
+        # if key is x then decrement tile index but not below 0
+        elif key == ord('y'):
+            tile_index = max(0, tile_index - 1)
+            i, tile_matrix = player.hand[tile_index]
+        # if key is y then increment tile index but not above the length of the hand
+        elif key == ord('x'):
+            tile_index = min(len(player.hand) - 1, tile_index + 1)
+            i, tile_matrix = player.hand[tile_index]
+        # if key is m then mirror the tile
+        elif key == ord('m'):
+            tile_matrix = Tile.mirror_tile(tile_matrix, vertical=True)
+            
         elif key == ord('q'):
             break
         
@@ -74,7 +89,10 @@ def print_welcome_message(stdscr):
     stdscr.addstr(2, 0, "Press 'q' to quit", curses.color_pair(3))
     stdscr.addstr(3, 0, "Press 'space' to rotate the tile", curses.color_pair(4))
     stdscr.addstr(4, 0, "Press 'Enter' to place the tile", curses.color_pair(5))
-    stdscr.addstr(5, 0, "Press 'space' to start", curses.color_pair(6))
+    stdscr.addstr(5, 0, "Press 'y' to select the previous tile", curses.color_pair(6))
+    stdscr.addstr(6, 0, "Press 'x' to select the next tile", curses.color_pair(1))
+    stdscr.addstr(7, 0, "Press 'm' to mirror the tile", curses.color_pair(2))
+    stdscr.addstr(8, 0, "Press 'space' to start", curses.color_pair(3))
        
     stdscr.refresh()
     
